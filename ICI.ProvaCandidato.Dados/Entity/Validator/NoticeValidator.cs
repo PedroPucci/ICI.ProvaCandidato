2a456614736e84748345d611876c909e6cc8af24
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-03-30 21:03:41 -0300

    validacao

---
+++ ICI.ProvaCandidato.Dados/Entity/Validator/NoticeValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using ICI.ProvaCandidato.Web.Models;
+
+namespace ICI.ProvaCandidato.Dados.Entity.Validator
+{
+    public class NoticeValidator : AbstractValidator<NoticeEntity>
+    {
+        public NoticeValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty();
+
+            RuleFor(x => x.Title)
+                .NotEmpty();
+
+            RuleFor(x => x.Text)
+                .NotEmpty();
+        }
+    }
+}
