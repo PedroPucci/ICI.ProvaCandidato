95cf783644052038adb92e60cbb71179bb2c9b09
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-03-31 11:03:57 -0300

    bd

---
+++ ICI.ProvaCandidato.Dados/Migrations/20240330034009_inicial.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ICI.ProvaCandidato.Dados.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TagEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Mail = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoticeEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    UserModelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoticeEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NoticeEntity_UserModel_UserModelId",
+                        column: x => x.UserModelId,
+                        principalTable: "UserModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoticeTagEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NoticeModelId = table.Column<int>(type: "integer", nullable: true),
+                    TagEntityId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoticeTagEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NoticeTagEntity_NoticeEntity_NoticeModelId",
+                        column: x => x.NoticeModelId,
+                        principalTable: "NoticeEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_NoticeTagEntity_TagEntity_TagEntityId",
+                        column: x => x.TagEntityId,
+                        principalTable: "TagEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoticeEntity_UserModelId",
+                table: "NoticeEntity",
+                column: "UserModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoticeTagEntity_NoticeModelId",
+                table: "NoticeTagEntity",
+                column: "NoticeModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoticeTagEntity_TagEntityId",
+                table: "NoticeTagEntity",
+                column: "TagEntityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NoticeTagEntity");
+
+            migrationBuilder.DropTable(
+                name: "NoticeEntity");
+
+            migrationBuilder.DropTable(
+                name: "TagEntity");
+
+            migrationBuilder.DropTable(
+                name: "UserModel");
+        }
+    }
+}
